15adbcbcb9f16936a32ee0eedfe43e475e39c723
@thomas_rz <thomas.daem@me.com>
2016-02-17 14:10:53 +0100

    templates

--- src/js/config/templates.js
+++ src/js/config/templates.js
@@ -1424,21 +1424,14 @@ var templates = [
                 }
             },
             {
-                "id": "appleWatch",
-                "title": "Activity Gauge (Apple Watch)",
+                "id": "appleWatchIsh",
+                "title": "Activity Gauge",
                 "description": "",
                 "definition": {
 
                     chart: {
                         type: 'solidgauge',
                         marginTop: 50
-                    },
-
-                    title: {
-                        text: 'Activity',
-                        style: {
-                            fontSize: '24px'
-                        }
                     },
 
                     tooltip: {
@@ -1448,13 +1441,117 @@ var templates = [
                         style: {
                             fontSize: '16px'
                         },
-                        pointFormat: '{series.name}<br><span style="font-size:2em; color: {point.color}; font-weight: bold">{point.y}%</span>',
                         positioner: function (labelWidth, labelHeight) {
                             return {
                                 x: 200 - labelWidth / 2,
                                 y: 180
                             };
+                        },
+                        pointFormat: '{series.name}<br><span style="font-size:2em; color: {point.color}; font-weight: bold">{point.y}%</span>'
+                    },
+
+                    pane: {
+                        startAngle: 0,
+                        endAngle: 360,
+                        background: [{ // Track for Move
+                            outerRadius: '112%',
+                            innerRadius: '88%',
+                            backgroundColor: Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0.3).get(),
+                            borderWidth: 0
+                        }, { // Track for Exercise
+                            outerRadius: '87%',
+                            innerRadius: '63%',
+                            backgroundColor: Highcharts.Color(Highcharts.getOptions().colors[1]).setOpacity(0.3).get(),
+                            borderWidth: 0
+                        }, { // Track for Stand
+                            outerRadius: '62%',
+                            innerRadius: '38%',
+                            backgroundColor: Highcharts.Color(Highcharts.getOptions().colors[2]).setOpacity(0.3).get(),
+                            borderWidth: 0
+                        }]
+                    },
+
+                    yAxis: {
+                        min: 0,
+                        max: 100,
+                        lineWidth: 0,
+                        tickPositions: []
+                    },
+
+                    plotOptions: {
+                        solidgauge: {
+                            borderWidth: '34px',
+                            dataLabels: {
+                                enabled: false
+                            },
+                            linecap: 'round',
+                            stickyTracking: false
                         }
+                    },
+
+                    series: [{
+                        borderColor: Highcharts.getOptions().colors[0],
+                        data: {
+                            color: Highcharts.getOptions().colors[0],
+                            radius: '100%',
+                            innerRadius: '100%'
+
+                        }
+                    }, {
+
+                        borderColor: Highcharts.getOptions().colors[1],
+                        data: {
+                            color: Highcharts.getOptions().colors[1],
+                            radius: '75%',
+                            innerRadius: '75%'
+                        }
+                    }, {
+                        borderColor: Highcharts.getOptions().colors[2],
+                        data: {
+                            color: Highcharts.getOptions().colors[2],
+                            radius: '50%',
+                            innerRadius: '50%'
+                        }
+                    }]
+
+                }
+            },
+            {
+                "id": "appleWatch",
+                "title": "Activity Gauge (Apple Watch)",
+                "description": "",
+                "definition": {
+                    chart: {
+                        type: 'solidgauge',
+                        marginTop: 50,
+                        backgroundColor: 'black',
+                        width:400
+                    },
+                    //colors: ['#F62366', '#9DFF02', '#0CCDD6'],
+
+                    title: {
+                        text: 'Activity',
+                        style: {
+                            fontSize: '24px',
+                            color: 'silver'
+                        }
+                    },
+
+                    tooltip: {
+                        borderWidth: 0,
+                        backgroundColor: 'none',
+                        shadow: false,
+                        style: {
+                            fontSize: '16px',
+                            color: 'silver'
+                        },
+                        positioner: function (labelWidth, labelHeight) {
+                            return {
+                                x: 200 - labelWidth / 2,
+                                y: 180
+                            };
+                        },
+                        pointFormat: '{series.name}<br><span style="font-size:2em; color: {point.color}; font-weight: bold">{point.y}%</span>'
                     },
 
                     pane: {
