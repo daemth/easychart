80237a6f5bbf0d634e781330d1e682e838d8016d
@thomas_rz <thomas.daem@me.com>
2016-04-15 12:52:50 +0200

    no message

--- src/js/factories/properties/arrayArray.js
+++ src/js/factories/properties/arrayArray.js
@@ -12,43 +12,42 @@
 
     function constructor(property, configService, configValue, propertyService) {
         var list = [];
-
-
-        console.log(property);
-        console.log(configService.get());
-        console.log(configValue);
-
-
-        //var values = _.merge(_.cloneDeep(property.defaults), configValue, []);
         var values = _.cloneDeep(configValue) || [];
 
         _.forEach(configValue, function (value, index) {
             console.log(configValue);
             var optionsList = [];
             var title = h('div.title', [h('h4', '' + (index + 1)), removeButton(property.fullname + '.' + index)]);
+
             _.forEach(property.options, function (option) {
-                //var fullnamePartial = option.fullname.split(property.title);
-                //optionsList.push(propertyService.get(option, configService, property.fullname)[])
-                // console.log(option);
                 optionsList.push(_.join(option, ","));
             });
-            list.push(h('div.item', [title,
-                h('input', {
-                        type: 'text',
-                        'ev-input': function (e) {
-                            values[index] = e.target.value != '' ? _.split(e.target.value, ",") : property.defaults[index];
-                            //console.log(values);
-                            /*if (e.target.value !== '') {
-                             //configService.setValue(property.fullname, e.target.value);
-                             } else {
-                             //configService.removeValue(property.fullname);
-                             }
-                             */
-                            configService.setValue(property.fullname, values);
+
+            list.push(h('div.form-item', [
+                h('div.form-item__label', [
+                    h('label', {
+                        'ev-click': function (e) {
+                            if (!disabled) {
+                                e.target.parentNode.parentNode.querySelector('input').focus(); // todo
+                            }
                         }
-                        , value: _.join(value, ",")
-                    }
-                )]))
+                    }, index)
+                ]),
+                h('div.form-item__input', [
+                    h('input', {
+                            type: 'text',
+                            'ev-input': function (e) {
+                                values[index] = e.target.value != '' ? _.split(e.target.value, ",") : property.defaults[index];
+                                configService.setValue(property.fullname, values);
+                            }
+                            , value: _.join(value, ",")
+                        }
+                    )
+                ]),
+                removeButton(property.fullname + '.' + index)
+            ]));
+
+
         });
 
         function removeButton(name) {
