9ceb22f5b0f51a873792db4a96834807fabb2bb1
Thomas O. Daem <thomas.daem@me.com>
2016-09-17 17:33:15 +0200

    events added to app-build with UIMode-option "render" initializer adapted to handle UIMode-options

--- src/js/app.js
+++ src/js/app.js
@@ -3,96 +3,101 @@ var Delegator = require("dom-delegator");
 Delegator();
 
 function constructor(opts) {
+    var mediator = require('mediatorjs');
+    var mInstance = new mediator.Mediator();
+    var initializer = require('./services/initializer');
+
+    var dataService = require('./services/data');
+    var confService = require('./services/config');
+    var data = new dataService(mInstance);
+    var config = new confService(mInstance, data);
+
+
     switch (opts.UIMode) {
         case 'render':
-            var dataService = require('./services/data');
-            var confService = require('./services/config');
-            var mediator = require('mediatorjs');
-            var mInstance = new mediator.Mediator();
-            var data = new dataService(mInstance);
-            var config = new confService(mInstance, data);
             var services = {
                 data: data,
                 config: new confService(mInstance, data),
                 mediator: mInstance
             };
 
+            // initialize the application
+            initializer(opts, services);
+
             if (typeof opts.data !== 'undefined') {
                 services.data.set(opts.data);
             }
-
             if (typeof opts.dataUrl !== 'undefined') {
                 services.data.setUrl(opts.dataUrl);
             }
-
             if (typeof opts.config !== 'undefined') {
                 services.config.set(opts.config);
             }
             if (typeof opts.presets !== 'undefined') {
                 services.config.setPresets(opts.presets);
             }
-
             if (typeof opts.element !== 'undefined') {
                 opts.element.className += ' ec';
                 var chart = require('./components/chart.js');
                 chart.load(opts.element, services);
             }
 
-        function setData(data) {
-            services.data.set(data);
-        }
+            function setData(data) {
+                services.data.set(data);
+            }
 
-        function setDataUrl() {
-            services.data.setUrl(url);
-        }
+            function setDataUrl() {
+                services.data.setUrl(url);
+            }
 
             // config
-        function setConfig(config) {
-            services.config.set(config);
-        }
+            function setConfig(config) {
+                services.config.set(config);
+            }
 
-        function setConfigStringified(string) {
-            services.config.setStringified(string);
-        }
+            function setConfigStringified(string) {
+                services.config.setStringified(string);
+            }
 
-        function getConfig(config) {
-            return services.config.getRaw(config);
-        }
+            function getConfig(config) {
+                return services.config.getRaw(config);
+            }
 
-        function getConfigStringified() {
-            return services.config.getStringified();
-        }
+            function getConfigStringified() {
+                return services.config.getStringified();
+            }
 
-        function setPresets(presets) {
-            services.config.setPresets(presets);
-        }
+            function setPresets(presets) {
+                services.config.setPresets(presets);
+            }
+
+            // events
+            function on(event, callback) {
+                services.mediator.on(event, function (data) {
+                    callback(data);
+                });
+            }
 
             return {
-                setData: setData,
-                setDataUrl: setDataUrl,
                 setConfig: setConfig,
                 setConfigStringified: setConfigStringified,
+                setData: setData,
+                setDataUrl: setDataUrl,
+                setPresets: setPresets,
                 getConfig: getConfig,
                 getConfigStringified: getConfigStringified,
-                setPresets: setPresets
+                on: on
             };
 
-
+        // app-build
         default:
-
             var router = require('./services/router.js');
-            var dataService = require('./services/data');
-            var confService = require('./services/config');
             var optionsService = require('./services/options');
             var revisionService = require('./services/revision');
             var templateService = require('./services/templates');
-            var initializer = require('./services/initializer');
+            //var initializer = require('./services/initializer');
             var Api = require('./services/api');
-            var mediator = require('mediatorjs');
             var h = require('virtual-dom/h');
-            var mInstance = new mediator.Mediator();
-            var data = new dataService(mInstance);
-            var config = new confService(mInstance, data);
 
             var services = {
                 data: data,
@@ -167,8 +172,9 @@ function constructor(opts) {
                 }
             }
 
-            // initialise the application with given options
+            // initialize the application with given options
             initializer(opts, services);
+            
             if (typeof opts.element !== 'undefined') {
                 opts.element.className += ' ec';
                 var mainRouter = new router(opts.element, states, services, opts.showLogo !== false);
